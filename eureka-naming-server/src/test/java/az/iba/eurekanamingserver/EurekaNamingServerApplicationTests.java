f2abe0565804fd8d40ff4855b7ff6e20fa1ad865
Susan Yunusova <46674284+SusanYunusova@users.noreply.github.com>
2020-08-04 14:19:01 +0400

    Add files via upload

---
+++ eureka-naming-server/src/test/java/az/iba/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package az.iba.eurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
